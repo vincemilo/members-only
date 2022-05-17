e0c2e8c8966c1b96ed1df3b79086d7e19b109279
vincemilo <vincemilo@gmail.com>
2022-05-17 02:45:24 -0600

    added foreign key to posts and relations to user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
+    @post = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 end
