6c202e425259ff4244b99bedb865467c6ebe13af
vincemilo <vincemilo@gmail.com>
2022-05-17 21:11:13 -0600

    finally got sign out to work after some finagling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
   def index
-    @post = Post.all
+    @posts = Post.all
   end
 
   def new
@@ -18,5 +18,18 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
